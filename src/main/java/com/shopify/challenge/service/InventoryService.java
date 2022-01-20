a3d2196a109d9dcca24a3db2fb5f4c7f659ec2cc
Ali Ahad Badruddin <alyhad.jariya@gmail.com>
2022-01-19 20:58:53 -0500

    Created API to get all items as CSV

--- src/main/java/com/shopify/challenge/service/InventoryService.java
+++ src/main/java/com/shopify/challenge/service/InventoryService.java
@@ -4,9 +4,24 @@ import com.shopify.challenge.entity.Item;
 import com.shopify.challenge.exception.ItemNotFoundException;
 import com.shopify.challenge.repository.InventoryRepository;
 import lombok.AllArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -42,6 +57,58 @@ public class InventoryService {
 
     public void deleteItem(Long id) {
         inventoryRepository.deleteById(id);
+    }
+
+    public ResponseEntity<Resource> getAllItemsAsCsv() {
+        String[] csvHeader = Arrays.stream(Item.class.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.toList())
+                .toArray(String[]::new);
+
+        List<List<String>> csvBody = new ArrayList<>();
+
+        inventoryRepository.findAll().forEach(
+                item -> csvBody.add(
+                        Arrays.asList(
+                                item.getId().toString(),
+                                item.getName(),
+                                item.getPrice().toString()
+                        )
+                ));
+
+        ByteArrayInputStream byteArrayOutputStream;
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            CSVPrinter csvPrinter = new CSVPrinter(
+                    new PrintWriter(out),
+                    CSVFormat.DEFAULT.withHeader(csvHeader)
+            );
+
+            for (List<String> record : csvBody) {
+                csvPrinter.printRecord(record);
+            }
+
+            csvPrinter.flush();
+
+            byteArrayOutputStream = new ByteArrayInputStream(out.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        InputStreamResource fileInputStream = new InputStreamResource(byteArrayOutputStream);
+
+        String csvFileName = "items.csv";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + csvFileName);
+        headers.set(HttpHeaders.CONTENT_TYPE, "text/csv");
+
+        return new ResponseEntity<>(
+                fileInputStream,
+                headers,
+                HttpStatus.OK
+        );
     }
 
 }
