9c7a9ad44489ade9011aa4716c7282f5086b4eda
Ali Ahad Badruddin <alyhad.jariya@gmail.com>
2022-01-19 20:50:57 -0500

    Create API to get item by id

--- src/main/java/com/shopify/challenge/service/InventoryService.java
+++ src/main/java/com/shopify/challenge/service/InventoryService.java
@@ -1,6 +1,7 @@
 package com.shopify.challenge.service;
 
 import com.shopify.challenge.entity.Item;
+import com.shopify.challenge.exception.ItemNotFoundException;
 import com.shopify.challenge.repository.InventoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,11 @@ public class InventoryService {
 
     public List<Item> getAllItems() {
         return inventoryRepository.findAll();
+    }
+
+    public Item getItemById(Long id) {
+        return inventoryRepository.findById(id)
+                .orElseThrow(() -> new ItemNotFoundException(id));
     }
 
 }
