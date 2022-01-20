553f50469224f4c8716fcef604f20205be5a3d16
Ali Ahad Badruddin <alyhad.jariya@gmail.com>
2022-01-19 20:48:59 -0500

    Create API to get all items

--- src/main/java/com/shopify/challenge/controller/InventoryController.java
+++ src/main/java/com/shopify/challenge/controller/InventoryController.java
@@ -1,12 +1,24 @@
 package com.shopify.challenge.controller;
 
+import com.shopify.challenge.entity.Item;
+import com.shopify.challenge.service.InventoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/items")
+    public List<Item> getAllItems() {
+        return inventoryService.getAllItems();
+    }
 
 }
